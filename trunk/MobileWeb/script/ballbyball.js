7cdd6dec60790e80356fa08dcbfc3fac0601b2b9
grahampontin <grahampontin@1587f532-6000-11de-ad4a-3d330faba797>
2012-02-07 21:51:43 +0000

    ball by ball checkpoint commit

--- trunk/MobileWeb/script/ballbyball.js
+++ trunk/MobileWeb/script/ballbyball.js
@@ -1,9 +1,66 @@
 ﻿$(document).ready(
 		function () {
-		    //Bind labels to input boxes
-		    $("input").labelify();
-		    $(".datePicker").datepicker({ dateFormat: 'dd MM yy' });
-		    $("button").button();
-
+		    addBall(new ball("4", "wd"));
+		    addBall(new ball("2", ""));
+		    addBall(new ball("0", ""));
+		    addBall(new ball("4", ""));
+		    addBall(new ball("0", ""));
+		    addBall(new ball("0", "w"));
+            
+		    write();
 		}
 	);
+
+function addBall(aball) {
+	over.balls.push(aball);
+}
+
+function write() {
+    $("#overSoFar").html(over.toPrettyString());
+}
+
+var over = new Over();
+
+function ball(number, thing) {
+    this.amount = number;
+    this.thing = thing;
+    this.toBallString = toBallString;
+}
+
+function toBallString() {
+    var output = "";
+    switch (this.thing) {
+        case "w":
+            output = "W";
+            break;
+        case "":
+            if (this.amount == 0) {
+                output = ".";
+            } else {
+                output = this.amount;
+            }
+            break;
+        default:
+            output = this.amount + this.thing;
+
+    }
+    return output;
+}
+
+function Over() {
+    this.balls = new Array();
+    this.toPrettyString = toPrettyString;
+}
+
+function toPrettyString() {
+    var output = "";
+    for (i = 0; i < this.balls.length; i++) {
+        var ball = this.balls[i];
+        output += this.balls[i].toBallString() + " ";
+    }
+    return output;
+}
+
+
+
+
