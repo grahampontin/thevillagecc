fa761a1f2c3b216d61293f68414daa9caddd5ea2
Graham Pontin <grahampontin@gmail.com>
2017-06-11 21:47:08 +0100

    help page

---
+++ MobileWeb/script/ballbyball.help.js
@@ -0,0 +1,23 @@
+function initialiseHelp() {
+    $("#deleteLastOver").click(function () {
+        $("#deleteLastOver").prop('disabled', true);
+        var postData = { 'command': "deleteLastOver", 'matchId': matchState.MatchId, 'payload': '{}' };
+        $.post('./CommandHandler.ashx', JSON.stringify(postData), function (data) {
+            //success
+            matchState = matchStateFromData(data);
+            $("body").pagecontainer("change", "BallByBall.aspx", { transition: "fade" });
+            write();
+        }, 'json')
+         .fail(function (data) {
+             showError(data.responseText);
+         });
+    });
+    $("#forceReload").click(function () {
+        $("#forceReload").prop('disabled', true);
+        window.location.replace("/MobileWeb/BallByBall/BallByBall.aspx?matchId=" + matchState.MatchId);
+    });
+    $("#backToMatchSelect").click(function () {
+        $("#backToMatchSelect").prop('disabled', true);
+        window.location.replace("/MobileWeb/BallByBall/SelectMatch.aspx");
+    });
+};
